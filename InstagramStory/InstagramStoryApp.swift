7713f700b975769296abb1bb0c90023e22826c73
meet74 <meet237@gmail.com>
2023-11-07 14:17:59 -0500

    Initial Commit

---
+++ InstagramStory/InstagramStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramStoryApp.swift
+//  InstagramStory
+//
+//  Created by Meet Patel on 2023-11-07.
+//
+
+import SwiftUI
+
+@main
+struct InstagramStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
